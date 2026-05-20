b7f2f4f762e25b36993531c10d4950a80ebea44f
limbo2341 <igorgrabovetc21@gmail.com>
2026-05-20 12:10:18 +0300

    fix: add search_all_stores to services __init__

--- bot/services/__init__.py
+++ bot/services/__init__.py
@@ -1,11 +1,9 @@
-from .ai_service import analyze_game_description, analyze_screenshot_description, get_refined_search
-from .rawg_service import search_games, get_game_details, format_game_card
+from .ai_service import analyze_game_description, analyze_screenshot_description, get_refined_search, search_all_stores, format_game_card
 
 __all__ = [
     "analyze_game_description",
     "analyze_screenshot_description",
     "get_refined_search",
-    "search_games",
-    "get_game_details",
+    "search_all_stores",
     "format_game_card",
 ]
